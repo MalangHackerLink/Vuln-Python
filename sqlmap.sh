ab27eba7b2e19dae6f9d2dfb3253a17331c41a39
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2025-05-31 11:07:33 +0700

    Update sqlmap.sh

--- sqlmap.sh
+++ sqlmap.sh
@@ -1,1 +1,1 @@
-python sqlmap.py -u "http://127.0.0.1:5000/api/notes/view" --method=POST --headers="Content-Type: application/json" --headers "Authorization: ..." --data='{"id": "1"}' -p id --dbs --batch
+python sqlmap.py -u "http://127.0.0.1:5000/api/notes/view" --method=POST --headers="Content-Type: application/json\nAuthorization: Bearer ..." --data='{"id": "1"}' -p id --dbs --batch
